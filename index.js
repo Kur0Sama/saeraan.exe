53d191eec3cb4e89946cf33587a8237dd844bfd2
Kur0Sama <32812112+Kur0Sama@users.noreply.github.com>
2017-10-15 12:19:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+// IMPORTATIONS
+
+const Discord = require('discord.js');
+const fs = require('fs');
+const ffmpeg = require('ffmpeg');
+const YTDL = require('ytdl-core');
+const opusscript = require('opusscript');
+
+// BOT
+
+const bot = new Discord.Client();
+
+bot.loadAll = require('./Fonctions/loadAll.js');
+bot.log = require('./Fonctions/log.js');
+bot.sendError = require('./Fonctions/sendErrorEmbed.js');
+bot.playSong = require('./Fonctions/playSong.js');
+
+bot.config = require('./parametres.json');
+bot.prefix = bot.config.prefixe;
+
+// FICHIERS
+
+let userData = JSON.parse(fs.readFileSync("Stockage/userData.json", "UTF-8"));
+
+// ACTIONS
+
+bot.loadAll(bot);
+
+// EVENNEMENTS
+
+bot.on('message', message => require('./Evenements/message.js')(bot, message));
+bot.on('guildMemberAdd', member => require('./Evenements/guildMemberAdd.js')(bot, member));
+bot.on('guildMemberQuit', member => require('./Evenements/guildMemberQuit.js')(bot, member));
+//bot.on('messageReactionAdd', (reaction, user) => require('./Evenements/messageReactionAdd.js')(bot, reaction, user));
+bot.on('ready', () => require('./Evenements/ready.js')(bot));
+
+// LOGIN
+
+bot.login(bot.config.token);
